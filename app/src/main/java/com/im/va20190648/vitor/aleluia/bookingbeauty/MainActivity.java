7a0ec408d426cc1400d753deb9b22c3000c11243
VitorAleluia <vitor.aleluia@ipcbcampus.pt>
2022-04-26 19:03:43 +0100

    Primeiro commit

---
+++ app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.im.va20190648.vitor.aleluia.bookingbeauty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
