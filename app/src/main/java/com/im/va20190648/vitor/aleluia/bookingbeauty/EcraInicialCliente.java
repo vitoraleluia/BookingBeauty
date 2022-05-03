2604b578ace9cbd4db0764fe35f35e094c9efc3c
VitorAleluia <vitor.aleluia@ipcbcampus.pt>
2022-05-03 17:01:41 +0100

    Adciionado interface do cliente

--- app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/EcraInicialCliente.java
+++ app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/EcraInicialCliente.java
@@ -2,17 +2,13 @@ package com.im.va20190648.vitor.aleluia.bookingbeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EcraInicialCliente extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent i = new Intent(this, EcraInicialCliente.class);
-        startActivity(i);
+        setContentView(R.layout.activity_ecra_inicial_cliente);
     }
 }
