8fb5564108ea851ec6870c0aebf64d900c334167
VitorAleluia <vitor.aleluia@ipcbcampus.pt>
2022-05-10 19:20:24 +0100

    Implementada a query de todos os servicos

--- app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/Servicos.java
+++ app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/Servicos.java
@@ -1,14 +1,52 @@
 package com.im.va20190648.vitor.aleluia.bookingbeauty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Servicos extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference servicos;
+    ListenerRegistration registration=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicos);
+
+        //ligacao com a bd
+        servicos = db.collection("servicos");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Ir buscar todos os servicos
+        if (registration != null)
+            registration.remove();
+
+        servicos.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());//Listar os servicos todos
+                    }
+                }else{
+
+                }
+            }
+        });
     }
 }
