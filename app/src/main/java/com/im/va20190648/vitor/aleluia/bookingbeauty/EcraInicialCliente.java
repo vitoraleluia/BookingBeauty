04f3fb2bf9e17080f608809865b0933de29f1da2
VitorAleluia <vitor.aleluia@ipcbcampus.pt>
2022-05-10 18:32:29 +0100

    Adicionado link entre as atividades

--- app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/EcraInicialCliente.java
+++ app/src/main/java/com/im/va20190648/vitor/aleluia/bookingbeauty/EcraInicialCliente.java
@@ -2,13 +2,22 @@ package com.im.va20190648.vitor.aleluia.bookingbeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EcraInicialCliente extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecra_inicial_cliente);
+    }
+
+    public void onClickServicos(View v){
+        Intent servicos = new Intent(EcraInicialCliente.this, Servicos.class);
+        startActivity(servicos);
     }
 }
